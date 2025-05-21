6fc6188ff4228721e0265d6125e6b041d104bece
lenakam1975 <lee2024skypro@gmail.com>
2025-05-21 09:20:58 +0700

    Отредактировала папку Readmy

---
+++ test/test_api.py
@@ -0,0 +1,126 @@
+import pytest
+import allure
+
+
+@pytest.mark.parametrize("expected_status", [200])
+@allure.id("Api-1")
+@allure.severity("critical")
+@allure.title("Поиск фильма по названию")
+@allure.feature("API. Поиск фильма")
+@allure.description("API тестирование.Поиск фильма по названию")
+def test_get_search_to_name(expected_status, auth_api):
+    with allure.step("Отправляем GET запрос. Поиск фильма по названию"):
+        search_to_name = auth_api.get_search_to_name("Вий")
+
+    with allure.step("Ожидаем статус == 200"):
+        assert expected_status == 200
+
+    with allure.step("Ожидаемый результат должен быть словарь dict"):
+        assert isinstance(search_to_name, dict)
+
+    with allure.step("Логируем полученные данные"):
+        print(f"Полученные данные: {search_to_name}, "
+              f"Статус: {expected_status}")
+
+
+@pytest.mark.parametrize("expected_status", [200])
+@allure.id("Api-2")
+@allure.severity("Major")
+@allure.title("Поиск фильма по дате премьеры в России")
+@allure.feature("API. Поиск фильма")
+@allure.description("API тестирование.Поиск фильма дате премьеры в России")
+def test_get_search_to_data(expected_status, auth_api):
+    with allure.step("Отправляем GET запрос. "
+                     "Поиск фильма по дате премьеры в России"):
+        search_to_data = auth_api.get_search_to_data("01.01.1997")
+
+    with allure.step("Ожидаем статус == 200"):
+        assert expected_status == 200
+
+    with allure.step("Ожидаемый результат должен быть словарь dict"):
+        assert isinstance(search_to_data, dict)
+        assert len(search_to_data) > 0
+
+    with allure.step("Логируем полученные данные"):
+        print(f"Полученные данные: {search_to_data}, "
+              f"Статус: {expected_status}")
+
+
+@pytest.mark.parametrize("expected_status", [200])
+@allure.id("Api-3")
+@allure.severity("critical")
+@allure.title("Поиск фильма по возрастному рейтингу")
+@allure.feature("API. Поиск фильма")
+@allure.description("API тестирование.Поиск фильма по возрастному рейтингу")
+def test_get_search_to_age(expected_status, auth_api):
+    with allure.step("Отправляем GET запрос. "
+                     "Поиск фильма по дате премьеры в России"):
+        search_to_age = auth_api.get_search_to_age()
+
+    with allure.step("Ожидаем статус == 200"):
+        assert expected_status == 200
+
+    with allure.step("Ожидаемый результат должен быть словарь dict"):
+        assert isinstance(search_to_age, dict)
+        assert len(search_to_age) > 0
+
+    with allure.step("Логируем полученные данные"):
+        for movie in search_to_age['docs']:
+            print(
+                f"Статус: {expected_status}, "
+                f"Полученные данные: (ID: {movie['id']}, "
+                f"Название: {movie['name'] or movie['alternativeName']}, "
+                f"Год: {movie['year']}, "
+                f"Рейтинг KP: {movie['rating']['kp']})"
+            )
+
+
+@pytest.mark.parametrize("expected_status", [200])
+@allure.id("Api-4")
+@allure.severity("Minor")
+@allure.title("Поиск фильма по году")
+@allure.feature("API. Поиск фильма")
+@allure.description("API тестирование.Поиск фильма по году")
+def test_get_search_to_year(expected_status, auth_api):
+    with allure.step("Отправляем GET запрос. "
+                     "Поиск фильма по дате премьеры в России"):
+        search_to_year = auth_api.get_search_to_year()
+
+    with allure.step("Ожидаем статус == 200"):
+        assert expected_status == 200
+
+    with allure.step("Ожидаемый результат должен быть словарь dict"):
+        assert isinstance(search_to_year, dict)
+        assert len(search_to_year) > 0
+
+    with allure.step("Логируем полученные данные"):
+        print(f"Полученные данные: {search_to_year}, "
+              f"Статус: {expected_status}")
+
+
+@pytest.mark.parametrize("expected_status", [200])
+@allure.id("Api-5")
+@allure.severity("critical")
+@allure.title("Поиск фильма по рейтингу Кинопоиск")
+@allure.feature("API. Поиск фильма")
+@allure.description("API тестирование.Поиск фильма по рейтингу Кинопоиск")
+def test_get_search_to_rating(expected_status, auth_api):
+    with allure.step("Отправляем GET запрос. "
+                     "Поиск фильма по дате премьеры в России"):
+        search_to_rating = auth_api.get_search_to_rating()
+
+    with allure.step("Ожидаем статус == 200"):
+        assert expected_status == 200
+
+    with allure.step("Ожидаемый результат должен быть словарь dict"):
+        assert isinstance(search_to_rating, dict)
+        assert len(search_to_rating) > 0
+
+    with allure.step("Логируем полученные данные"):
+        for movie in search_to_rating['docs']:
+            print(
+                f"Статус: {expected_status}, "
+                f"Полученные данные: (ID: {movie['id']}, "
+                f"Название: {movie['name'] or movie['alternativeName']} "
+                f"Рейтинг KP: {movie['rating']['kp']})"
+            )
---
+++ test/test_ui.py
@@ -0,0 +1,94 @@
+import allure
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+@allure.id("MainPage-1")
+@allure.severity("blocker")
+@allure.title("Клик по кнопке 'Вход'")
+@allure.story("работа функциональности кнопки 'Вход'")
+@allure.feature("кнопка 'Вход'")
+@allure.description("Функциональность кнопки 'Вход' на главной странице")
+def test_button_entrance(main_page):
+    with allure.step("Нажать на кнопку 'Вход'"):
+        main_page.click_on_button()
+
+    with allure.step("Дождаться загрузки элемента на странице"):
+        wait = WebDriverWait(main_page.driver, 10)
+        new_element = wait.until(
+            EC.visibility_of_element_located((
+                By.CLASS_NAME, 'passp-auth-content'))
+        )
+        assert new_element.is_displayed(), "Элемент не появился"
+
+
+@allure.id("MainPage-2")
+@allure.severity("critical")
+@allure.title("Ввод текста в строку поиск")
+@allure.story("Поиск фильма в строке поиска")
+@allure.feature("строка 'Поиск'")
+@allure.description("Функциональность строки 'Поиск' на главной странице")
+def test_input(main_page):
+    with allure.step("Ввести название фильма в строку поиск"):
+        main_page.input_text("Вий")
+
+    with allure.step("Дождаться загрузки элемента на странице"):
+        wait = WebDriverWait(main_page.driver, 20)
+        new_element = wait.until(EC.visibility_of_element_located((
+            By.CLASS_NAME, 'styles_root___4K2i'))
+        )
+        assert new_element.is_displayed(), "Элемент не появился"
+
+
+@allure.id("NoveltiesPage-1")
+@allure.severity("critical")
+@allure.title("Выбор фильма в разделе 'Новинки'")
+@allure.story("Выбор фильма на странице по очередности")
+@allure.feature("поиск фильма в разделе 'Новинки'")
+@allure.description("Выбор фильма в разделе 'Новинки' и появление элемента 'Смотреть фильм'")
+def test_novelties(novelties_page):
+    with allure.step("Выбрать любой фильм, кликнув по нему"):
+        novelties_page.click_element(30)
+
+    with allure.step("Дождаться загрузки элемента на странице"):
+        wait = WebDriverWait(novelties_page.driver, 10)
+        new_element = wait.until(
+            EC.visibility_of_element_located((
+                By.XPATH, "//button[@data-test-id='MainButton_offer']")))
+        assert new_element.is_displayed(), "Элемент не появился"
+
+        button_text = new_element.text.strip()
+        assert button_text != "", f"Кнопка пустая или невидимая: '{button_text}'"
+
+        print(button_text)
+
+
+@allure.id("MainPage-3")
+@allure.severity("critical")
+@allure.title("Меню навигация на главной странице")
+@allure.story("работа функциональности меню навигации")
+@allure.feature("меню навигация")
+@allure.description("Переход на страницы поочередно в меню навигации")
+def test_link_to_page(main_page):
+    with allure.step("Переход поочередно на страницы в меню навигации"):
+        main_page.navigation_menu('styles_root__7mPJN styles_lightThemeItem__BSbZW')
+
+        assert True, "Тест выполнен"
+
+
+@allure.id("ChannelsPage-1")
+@allure.severity("critical")
+@allure.title("Вернуться на главную страницу")
+@allure.story("работа функциональности элемента 'Кинопоиск'")
+@allure.feature("элемент хедер 'Кинопоиск' ")
+@allure.description("Функциональность элемента 'Кинопоиск' расположенный в хедер")
+def test_return_to_main_page(main_page, channels_page):
+    with allure.step("Перейти на страницу 'Телеканалы' и нажать на элемент 'Кинопоиск'"):
+        channels_page.back_homepage()
+
+    with allure.step("Дождаться загрузки главной страницы"):
+        current_url = main_page.driver.current_url
+        assert current_url == "https://www.kinopoisk.ru/", (f""
+                                                            f"Ожидалось перенаправление на главную страницу,"
+                                                            f" получено: {current_url}")
